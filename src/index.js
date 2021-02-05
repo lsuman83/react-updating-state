a3682c4ee929e5feec98dc5959fbb3fb0be0c8df
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-02-04 21:41:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ClicketyClick from './components/ClickityClick'
+
 ReactDOM.render(
   <div>
-    Mount Components Here
+    <ClicketyClick />
   </div>,
   document.getElementById('root')
 );
